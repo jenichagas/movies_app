94892229defa90d72bf23947c28c13d3d7150dd7
Jeniffer <jenif.work@gmail.com>
2025-11-13 10:45:15 -0300

    upload inital project

---
+++ components/CardGrid/CardGrid.tsx
@@ -0,0 +1,7 @@
+export default function CardGrid() {
+  return (
+    <div>
+      <h1>CardGrid</h1>
+    </div>
+  );
+}
---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <div className="nav-bar">
+      <h1 className="nav-title">CINE BOX</h1>
+    </div>
+  );
+}
