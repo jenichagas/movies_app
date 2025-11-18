4720dcf1dffc535b42bd0291f36da430b53fee05
Jeniffer <jenif.work@gmail.com>
2025-11-18 16:37:54 -0300

    remove page responsibility for API lookup and filtering.

---
+++ app/[id]/page.tsx
@@ -0,0 +1,6 @@
+export default function Film() {
+  return <div className="">
+
+    
+  </div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import MovieGrid from "@/components/MovieGrid";
 import type { ApiMovieResponse, MovieProps } from "@/app/types";
-import HeroSection from "../components/HeroSection";
+import PageClient from "./PageClient";
 
-async function getMovies(): Promise<MovieProps[]> {
+export async function getPopulateMovies(): Promise<MovieProps[]> {
   const baseUrl = "https://api.themoviedb.org/3/discover/movie";
-  if (!process.env.API_KEY) {
+  if (!process.env.NEXT_PUBLIC_API_KEY) {
     throw new Error("API_KEY não encontrada");
   }
   const params = new URLSearchParams({
-    api_key: process.env.API_KEY,
+    api_key: process.env.NEXT_PUBLIC_API_KEY,
     language: "pt-BR",
   });
   const response = await fetch(`${baseUrl}?${params.toString()}`);
@@ -19,18 +18,26 @@ async function getMovies(): Promise<MovieProps[]> {
   return data.results;
 }
 
-export default async function Home() {
-  const movies = await getMovies();
-
-  return (
-    <div className="">
-      <div>
-        <HeroSection />
-      </div>
-      <div>
-        <MovieGrid movies={movies} />
-      </div>
-    </div>
-  );
+export async function searchMovies(query: string): Promise<MovieProps[]> {
+  const baseUrl = "https://api.themoviedb.org/3/search/movie";
+  if (!process.env.NEXT_PUBLIC_API_KEY) {
+    throw new Error("API_KEY não encontrada");
+  }
+  const params = new URLSearchParams({
+    api_key: process.env.NEXT_PUBLIC_API_KEY,
+    language: "pt-BR",
+    query: query,
+  });
+  const response = await fetch(`${baseUrl}?${params.toString()}`);
+  if (!response.ok) {
+    throw new Error("Erro ao buscar filmes");
+  }
+  const data = (await response.json()) as ApiMovieResponse;
+  return data.results;
 }
 
+export default async function Home() {
+  const initialMovies = await getPopulateMovies();
+
+  return <PageClient initialMovies={initialMovies} />;
+}
