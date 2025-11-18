f817a95090e308bebba8b61c2e63ac956f13b2d3
Jeniffer <jenif.work@gmail.com>
2025-11-18 10:49:33 -0300

    add home in navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,10 +1,18 @@
 import style from "./NavBar.module.scss";
 import Logo from "@/components/Logo";
+import Link from "next/link";
+import { TiHomeOutline } from "react-icons/ti";
 
 export default function NavBar() {
   return (
     <div className={style.navBar}>
       <Logo />
+      <Link href="/">
+        <div className={style.links}>
+          <TiHomeOutline />
+          <p>Início</p>
+        </div>
+      </Link>
     </div>
   );
 }
