9713d609e53aa111dd1ccccef769b2b17bf2cd72
Jeniffer <jenif.work@gmail.com>
2025-11-18 16:41:12 -0300

    implementation of search functionality in the movie list

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -3,22 +3,39 @@ import styles from "./Search.module.scss";
 import { IoSearchCircleSharp } from "react-icons/io5";
 import { useEffect, useState } from "react";
 
-  
+interface SearchProps {
+  onSearch: (query: string) => void;
+}
 
-
-
-export default function Search() {
+export default function Search({ onSearch }: SearchProps) {
   const [isMounted, setIsMounted] = useState(false);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className={styles.searchContainer}>
       {isMounted && <IoSearchCircleSharp />}
-      <input type="text" placeholder="Pesquise por um filme ou uma série..." />
-      <button>Pesquisar</button>
+      <input
+        type="text"
+        placeholder="Pesquise por um filme ou uma série..."
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch}>Pesquisar</button>
     </div>
   );
 }
