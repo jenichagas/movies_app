372ad5ac304481a61c5752771caeaee36fb1a57d
Jeniffer <jenif.work@gmail.com>
2025-11-24 11:56:01 -0300

    fix: correct formatting and structure of MovieProps interface in types.ts

--- app/types.ts
+++ app/types.ts
@@ -1,16 +1,17 @@
 export interface MovieProps {
   id: string;
   title: string;
-  overview:string
+  overview: string;
   poster_path: string;
   vote_average: number;
   vote_count: number;
-  release_date: string,
-  adult: boolean,
-  backdrop_path: string,
-  genre_ids: number[],
-  original_language: string,
-
+  release_date: string;
+  original_language: string;
+  genre_ids?: number[];
+  adult?: boolean;
+  genres?: { id: number; name: string }[];
+  runtime?: number;
+  tagline?: string;
 }
 
 export interface ApiMovieResponse {
@@ -19,3 +20,11 @@ export interface ApiMovieResponse {
   total_pages: number;
   total_results: number;
 }
+export interface ApiSeriesResponse {
+  page: number;
+  results: MovieProps[];
+  total_pages: number;
+  total_results: number;
+}
+
+
