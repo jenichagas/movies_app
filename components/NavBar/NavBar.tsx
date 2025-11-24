0b462ddc9aed63d0e1c43244d1758772e40f1e66
Jeniffer <jenif.work@gmail.com>
2025-11-24 11:47:17 -0300

    add user perfil and settings

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,18 +1,21 @@
 "use client";
-import style from "./NavBar.module.scss";
+
 import Logo from "@/components/Logo";
+import Dropdown from "../Dropdown";
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import { IoSettings } from "react-icons/io5";
 import userMascot from "@/public/user-mascot.json";
 import { useRef, useState } from "react";
+import style from "./NavBar.module.scss";
+import Link from "next/link";
 
 export default function NavBar() {
   const lottieRef = useRef<LottieRefCurrentProps>(null);
   const [isClicked, setIsClicked] = useState(false);
-  const DELAY_ENTRE_LOOPS = 2000; 
-
+  const DELAY_ENTRE_LOOPS = 2000;
   const handleAnimationComplete = () => {
     setTimeout(() => {
-      lottieRef.current?.play();
+      lottieRef.current?.goToAndPlay(0, true);
     }, DELAY_ENTRE_LOOPS);
   };
 
@@ -22,14 +25,14 @@ export default function NavBar() {
     setIsClicked(true);
     setTimeout(() => {
       setIsClicked(false);
-    }, 200); 
+    }, 200);
   };
 
   return (
     <div className={style.navBar}>
       <Logo />
       <div className={style.actions}>
-        <div className={style.userName}>Jeniffer</div>
+        <div className={style.userName}>Olá, Jeniffer!</div>
         <div
           className={`${style.userIcon} ${
             isClicked ? style.userIconClicked : ""
@@ -42,6 +45,16 @@ export default function NavBar() {
             loop={false}
             onComplete={handleAnimationComplete}
           />
+        </div>
+        <div className={style.settingsIcon}>
+          <Dropdown trigger={() => <IoSettings />}>
+            <div className={style.settingsMenu}>
+              <Link href="#"><p>Perfil</p></Link>
+              <Link href="#"><p>Minha Lista</p> </Link>
+              <hr />
+              <Link href="#"><p>Sair</p></Link>
+            </div>
+          </Dropdown>
         </div>
       </div>
     </div>
