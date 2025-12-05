93039453e010f6da95e591b6476bcb965e5cc98d
Jeniffer <jenif.work@gmail.com>
2025-12-05 14:35:16 -0300

    feat: wrap Logo component's Image in Link for navigation

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import styles from "./Logo.module.scss";
+import  Link  from "next/link";
 
 export default function Logo() {
   return (
     <div className={styles.logoContainer}>
-      <Image
-        src="/logo-inline.png"
-        alt="Logo"
-        width={170}
-        height={30}
-        loading="eager"
-        className={styles.logo}
-      />
+      <Link href="/">
+        <Image
+          src="/logo-inline.png"
+          alt="Logo"
+          width={170}
+          height={30}
+          loading="eager"
+          className={styles.logo}
+        />
+      </Link>
     </div>
   );
 }
