698203425610cc1b8d0c0940b793b31739dc196a
Jeniffer <jenif.work@gmail.com>
2025-12-05 14:33:26 -0300

    feat: add MenuSkeleton to Loading component for improved loading state representation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,13 @@
 import CategoriesListSkeleton from "@/components/CategoriesList/Skeleton";
 import HeroSectionSkeleton from "@/components/HeroSection/Skeleton";
+import MenuSkeleton from "@/components/Menu/Skeleton";
 import MovieGridSkeleton from "@/components/MovieGrid/Skeleton/MovieGridSkeleton";
+
 
 export default function Loading() {
   return (
     <>
+      <MenuSkeleton />
       <HeroSectionSkeleton />
       <CategoriesListSkeleton />
       <MovieGridSkeleton />
