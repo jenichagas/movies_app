836b6802428efab38bda82f84ff46f1a62e2790c
Jeniffer <jenif.work@gmail.com>
2025-12-05 14:36:05 -0300

    feat: add SocialTooltip component with social media icons and styles

---
+++ components/SocialTooltip/SocialTooltip.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import styles from "./SocialTooltip.module.scss";
+import {
+  FaInstagram,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
+
+export default function SocialTooltip() {
+  return (
+    <div className={styles.socialIconsContainer}>
+      <div className={`${styles.iconWrapper} ${styles.insta}`}>
+        <p className={styles.iconName}>Instagram</p>
+        <div className={`${styles.icon} ${styles.insta}`}>
+          <FaInstagram size={25} />
+        </div>
+      </div>
+      <div className={`${styles.iconWrapper} ${styles.linkedin}`}>
+        <p className={styles.iconName}>Linkedin</p>
+        <div className={`${styles.icon} ${styles.link}`}>
+          <FaLinkedinIn size={25} />
+        </div>
+      </div>
+      <div className={`${styles.iconWrapper} ${styles.whatsapp}`}>
+        <p className={styles.iconName}>WhatsApp</p>
+        <div className={`${styles.icon} ${styles.whats}`}>
+          <FaWhatsapp size={25} />
+        </div>
+      </div>
+      <div className={`${styles.iconWrapper} ${styles.youtube}`}>
+        <p className={styles.iconName}>YouTube</p>
+        <div className={`${styles.icon} ${styles.tube}`}>
+          <FaYoutube size={25} />
+        </div>
+      </div>
+    </div>
+  );
+}
