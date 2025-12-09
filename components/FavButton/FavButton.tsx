db3500e753c6a0b2a09f444167f21d8df7cc8213
Jeniffer <jenif.work@gmail.com>
2025-12-09 14:21:41 -0300

    rafactor for using into others components

--- components/FavButton/FavButton.tsx
+++ components/FavButton/FavButton.tsx
@@ -2,18 +2,30 @@
 
 import { useState } from "react";
 import styles from "./FavButton.module.scss";
+import clsx from "clsx";
 
 interface FavButtonProps {
   movieId: string;
   movieTitle: string;
+  fontSize?: string;
+  showText?: boolean;
 }
 
-export default function FavButton({ movieId, movieTitle }: FavButtonProps) {
+export default function FavButton({
+  movieId,
+  movieTitle,
+  fontSize,
+  showText = true,
+}: FavButtonProps) {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(event.target.checked);
   };
+
+  const containerClasses = clsx(styles.container, {
+    [styles.noText]: !showText,
+  });
 
   return (
     <div>
@@ -26,7 +38,7 @@ export default function FavButton({ movieId, movieTitle }: FavButtonProps) {
         type="checkbox"
         className={styles.favorite}
       />
-      <label className={styles.container} htmlFor="favorite">
+      <label className={containerClasses} htmlFor="favorite">
         <svg
           className="feather feather-heart"
           strokeLinejoin="round"
@@ -41,10 +53,12 @@ export default function FavButton({ movieId, movieTitle }: FavButtonProps) {
         >
           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
         </svg>
-        <div className={styles.action}>
-          <span className={styles.option1}>Adicionar aos favoritos</span>
-          <span className={styles.option2}>Adicionado aos favoritos</span>
-        </div>
+        {showText && (
+          <div className={styles.action} style={{ fontSize: fontSize }}>
+            <span className={styles.option1}>Adicionar à minha lista</span>
+            <span className={styles.option2}>Remover da minha lista</span>
+          </div>
+        )}
       </label>
     </div>
   );
