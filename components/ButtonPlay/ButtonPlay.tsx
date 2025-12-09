f569ce8d96a7e2ca55f52b24ea3fbfe97883489c
Jeniffer <jenif.work@gmail.com>
2025-12-09 14:13:38 -0300

    add variables in play btn

--- components/ButtonPlay/ButtonPlay.tsx
+++ components/ButtonPlay/ButtonPlay.tsx
@@ -5,10 +5,10 @@ export default function ButtonPlay() {
     <div>
       <button className={styles.buttonPlay}>
         <span>Assistir</span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 66 43">
-          <polygon points="39.58,4.46 44.11,0 66,21.5 44.11,43 39.58,38.54 56.94,21.5"></polygon>
-          <polygon points="19.79,4.46 24.32,0 46.21,21.5 24.32,43 19.79,38.54 37.15,21.5"></polygon>
-          <polygon points="0,4.46 4.53,0 26.42,21.5 4.53,43 0,38.54 17.36,21.5"></polygon>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 24">
+          <polygon points="38,3 52,12 38,21"></polygon>
+          <polygon points="18,3 32,12 18,21"></polygon>
+          <polygon points="-2,3 12,12 -2,21"></polygon>
         </svg>
       </button>
     </div>
