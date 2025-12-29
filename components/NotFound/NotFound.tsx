5515ff6d9e065a2c3cc8c028abe17cb4109065e9
Jeniffer <jenif.work@gmail.com>
2025-12-29 09:47:03 -0300

    add height prop and pseud element

--- components/NotFound/NotFound.tsx
+++ components/NotFound/NotFound.tsx
@@ -3,9 +3,13 @@ import Lottie from "lottie-react";
 import notFoundAnimation from "@/public/cat-playing-animation.json";
 import styles from "./NotFound.module.scss";
 
-export default function NotFound() {
+interface NotFoundProps {
+  height?: string;
+}
+
+export default function NotFound(height: NotFoundProps) {
   return (
-    <div className={styles.notFoundContainer}>
+    <div className={styles.notFoundContainer} style={{ height: height.height }}>
       <Lottie animationData={notFoundAnimation} loop={true} />
       <p>Ops! Nenhum resultado encontrado.</p>
     </div>
