1295edd43f430702aed483b94567324ed84d5619
Jeniffer <jenif.work@gmail.com>
2025-12-30 11:16:25 -0300

    Refactor MyListPage to implement session-based authentication and fetch favorite movies

--- app/(private)/myList/page.tsx
+++ app/(private)/myList/page.tsx
@@ -1,19 +1,29 @@
 import { getFavoriteMovies, getGenres } from "@/lib/movies";
 import MyListPageClient from "./MyListPageClient";
 import { MovieProps } from "@/app/types";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function MyListPage() {
-  // Simulação de dados de autenticação
-  const accountId = "YOUR_ACCOUNT_ID";
-  const sessionId = "YOUR_SESSION_ID";
+  const session = await getSession();
 
-  // Busca os filmes favoritos e os gêneros em paralelo
-  const [favoriteMoviesData, genresData] = await Promise.all([
-    getFavoriteMovies(accountId, sessionId),
+  if (!session.isLoggedIn) {
+    redirect("/login");
+  }
+
+  const accountId = session.account_id;
+  const sessionId = session.session_id;
+
+  const [favoriteMoviesResponse, genresResponse] = await Promise.all([
+    getFavoriteMovies(accountId!, sessionId!),
     getGenres(),
   ]);
 
-  // Adiciona a propriedade is_favorite a todos os filmes, pois todos são favoritos nesta página
+  const favoriteMoviesData = favoriteMoviesResponse.success
+    ? favoriteMoviesResponse.data
+    : [];
+  const genresData = genresResponse.success ? genresResponse.data : [];
+
   const favoriteMovies: MovieProps[] = favoriteMoviesData.map(movie => ({
     ...movie,
     is_favorite: true,
